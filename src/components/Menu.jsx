47d246199ea20fa2949cd5ba7092fcc581359804
Nekzus <85507589+Nekzus@users.noreply.github.com>
2023-09-21 16:56:44 -0300

    Readme

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ const Menu = ({ onClick }) => {
   return (
     <div className="z-[100] flex min-h-[50vh] items-center justify-center">
       <button
-        className="cursor-pointer rounded-[20px] border-none px-20 py-10 text-[2em] shadow-[rgba(0,0,0,0.5)0px_0px_60px]"
+        className="cursor-pointer rounded-[20px] border-none px-20 py-10 text-[2em] neon-purple"
         onClick={onClick}
       >
         Play Tetris
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ theme, addUtilities }) => {
+      const neonUtilities = {};
+      const colors = theme("colors");
+      for (const color in colors) {
+        if (typeof colors[color] === "object") {
+          const color1 = colors[color]["500"];
+          const color2 = colors[color]["700"];
+          neonUtilities[`.neon-${color}`] = {
+            boxShadow: `0 0 5px ${color1}, 0 0 20px ${color2}`,
+          };
+        }
+      }
+      addUtilities(neonUtilities);
+    }),
+  ],
 };
